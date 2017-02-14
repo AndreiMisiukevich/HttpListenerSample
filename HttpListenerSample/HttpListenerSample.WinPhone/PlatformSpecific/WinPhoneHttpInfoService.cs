a40c4aaee9f568d71a1d627f1f617dd6c6e82d10
INTOSOFT\Misiukevich <andrei.misiukevich@intosoft.by>
2017-02-14 18:47:00 +0300

    added winPhone sample

---
+++ HttpListenerSample/HttpListenerSample.WinPhone/PlatformSpecific/WinPhoneHttpInfoService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpListenerSample.WinPhone.PlatformSpecific;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WinPhoneHttpInfoService))]
+
+namespace HttpListenerSample.WinPhone.PlatformSpecific
+{
+    public class WinPhoneHttpInfoService : IHttpInfoService
+    {
+        public string IpAddress
+        {
+            get
+            {
+                try
+                {
+                    var ipAddress = new List<string>();
+                    var hosts = Windows.Networking.Connectivity.NetworkInformation.GetHostNames().ToList();
+                    foreach (var host in hosts)
+                    {
+                        var ip = host.DisplayName;
+                        ipAddress.Add(ip);
+                    }
+
+                    return ipAddress.LastOrDefault();
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
